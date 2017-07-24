096ce31bf9b1d06765c100a9246114dd1bdcbabb
Peeter Meos <peeterm@proekspert.ee>
2017-07-24 21:29:52 +0300

    Initial commit

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname + '/public/images')));
+app.use('/stylesheets', express.static(path.join(__dirname + '/public/stylesheets')));
 
 app.use('/', index);
 app.use('/users', users);
